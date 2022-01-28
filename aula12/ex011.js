d6095a1e27f5460912df8dea2789458b978d5d88
Marcelo Veras <marceloantonino.verass@gmail.com>
2022-01-28 14:57:34 -0300

    exercises

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 36
+
+if (idade < 16) {
+        console.log('NÃO VOTA')
+} else if (idade < 18) {
+        console.log('VOTO OPCIONAL')
+} else{
+        console.log('VOTO OBRIGATÓRIO')
+}
+
