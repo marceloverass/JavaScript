731f1b3c40ebd015ca8834f8b2af208d785eb1a9
Marcelo Veras <marceloantonino.verass@gmail.com>
2022-02-20 20:23:32 -0300

    exercicios

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+var c = 0
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6) 
+
+
+/* 
+            WHILE
+
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/*
+ console.log('TUDO BEM'?)
+ console.log('TUDO BEM'?)
+ console.log('TUDO BEM'?)
+ console.log('TUDO BEM'?)
+ console.log('TUDO BEM'?)
+ console.log('TUDO BEM'?)
+ console.log('TUDO BEM'?)
+*/
