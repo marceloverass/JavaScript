335c5dbf294fa8c9f85f23252976fac8d3c0e831
Marcelo Veras <marceloantonino.verass@gmail.com>
2022-02-21 23:46:45 -0300

    exercicios

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,6 +1,30 @@
 function contar() {
-    var inicio = window.document.querySelector('input#txtinicio')
-    var fim = window.document.querySelector('input#txtfim')
-    var passo = window.document.querySelector('input#passo')
-    for(var c= 0; c )
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = '[ERRO] Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            // CONTAGEM CRESCENTE
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            // CONTAGEM REGRESSIVA
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 }
